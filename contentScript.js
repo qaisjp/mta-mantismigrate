bbffffb56e75529b92689304912d12b5a9fb2ae1
Qais Patankar <qaisjp@gmail.com>
2018-09-21 01:09:08 +0100

    Add some stuff

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,110 @@
-function useTemplate(template) {
-    console.log(template);
+function buildGitHubBox(bugData) {
+    // Clone the note box
+    const notebox = document.querySelector("#bugnoteadd").parentElement;
+
+    const cloned = notebox.cloneNode(true)
+    
+    // Remove a bunch of stuff we don't want!
+    cloned.querySelectorAll("#addbugnote").outerHTML = "";
+
+    // Rename some stuff
+    cloned.querySelector("#bugnote_text").id = "githubissue_text";
+    
+    // Add style
+    cloned.classList.add("mantismigrate-fixed");
+    
+    // Set title of new box to summat else
+    const title = cloned.querySelector(".widget-title");
+    title.innerHTML = "New GitHub issue"
+
+    // Rejig private row to be a milestone checkbox
+    const private = cloned.querySelector("table>tbody>tr:first-child");
+    private.children[0].textContent = "Backlog milestone";
+    let milestoneNote = "Dunno";
+    if (bugData.status === "resolved" || bugData.status === "closed") {
+        milestoneNote = "Why are you migrating a closed issue?";
+    } else if (bugData.status === "confirmed") {
+        milestoneNote = "YES YES";
+    }
+    const checkbox = private.children[1].querySelector("span").parentElement;
+    const checkboxInput = checkbox.querySelector("input")
+    checkboxInput.id = "mantismigrate-backlogit";
+    checkboxInput.checked = (bugData.status === "confirmed");
+    checkbox.setAttribute("for", "mantismigrate-backlogit");
+    checkbox.querySelector("span").innerHTML = "<strong>" + milestoneNote + "</strong> (Rule of thumb: confirmed issues go into 'Backlog')";;
+
+    // Change note to "description"
+    const descrEl = cloned.querySelector("table>tbody>tr:nth-child(2)");
+    descrEl.children[0].innerHTML = "Description";
+
+    // Create a title obj
+    const titleEl = document.createElement("tr");
+    titleEl.innerHTML = `<th class=category>Title</th><td>
+    <input type=text class="input-sm" style="width:100% !important"></input></td>`
+    titleEl.querySelector("input").value = bugData.title;
+    descrEl.parentNode.insertBefore(titleEl, descrEl);
+
+    // Rejig attach files to be markdown guidance
+    const attach = cloned.querySelector("#bugnote-attach-files")
+    attach.id = "";
+    attach.children[0].innerHTML = "Guidance";
+    attach.children[1].innerHTML = `
+Use <a href="https://guides.github.com/features/mastering-markdown/">GitHub Flavoured Markdown</a>. Lists and code should be well formatted.
+<br>
+You can syntax-highlight code by providing the language after the backticks, like so: \`\`\`lua
+<br>
+<strong>If a bug is confirmed</strong>, set the milestone on GitHub to "Backlog".
+<br>
+<strong>Once GH issue made: </strong>Paste the issue url below and click "comment & close".
+    `;
+
+    // Finally append
+    notebox.parentElement.appendChild(cloned);
+
+    const box = {
+    };
+
+    return box;
+}
+
+async function useTemplate(template, label) {
+    const url = `https://raw.githubusercontent.com/multitheftauto/mtasa-blue/master/.github/ISSUE_TEMPLATE/${template}`
+    
+    let resp = await fetch(url);
+    
+    let val = await resp.text();
+
+    // Skip yaml header + two newlines, slice the string
+    const headerEnd = val.indexOf("---", 3) + 5;
+    val = val.slice(headerEnd);
+
+    // Get bug data
+    const bugData = getBugData();
+
+    // Build GitHub box
+    const box = buildGitHubBox(bugData);
+
+    // Build new url
+    const buildURL = (title, body, assignee, withMilestone) => {
+        let url = "https://github.com/multitheftauto/mtasa-blue/issues/new?"
+        if (withMilestone) {
+            url += "milestone=Backlog&";
+        }
+        if (label) {
+            url += `labels=${encodeURIComponent(label)}&`;
+        }
+        if (assignee) {
+            url += `assignee=${encodeURIComponent(assignee)}&`;
+        }
+        url += `title=${encodeURIComponent(title)}&`;
+        url += `title=${encodeURIComponent(body)}&`;
+    }
+    
+    // Set box callbacks
+    box.onCreateIssue = (title, body, withMilestone) => {
+        const url = buildURL(title, body, "qaisjp", withMilestone);
+        window.open(url);
+    }
 }
 
 function addActionButtons() {
@@ -17,11 +122,11 @@ function addActionButtons() {
     toolbox.appendChild(bugBtn);
 
     featBtn.addEventListener("click", e => {
-        useTemplate("feature_request.md");
+        useTemplate("feature_request.md", "enhancement");
     });
 
     bugBtn.addEventListener("click", e => {
-        useTemplate("bug_report.md");
+        useTemplate("bug_report.md", "bug");
     })
 }
 
@@ -69,3 +174,4 @@ function main() {
 }
 
 addEventListener("load", main);
+console.log("mantismigrate running");
